723507473a2a4323a9c49856a649d71a38a9dd6f
Nilsimon <nil.simon@campus.xaloc.org>
2024-09-13 12:33:12 +0200

    Add files via upload

---
+++ Programacion/python/ejemplos/variables_1.py
@@ -0,0 +1,11 @@
+#trabajaremos con 3 tipos de variables
+#en python el tipo de variable lo define el valor que lo ponga
+var_texto="hola"
+var_numero=3
+var_num=3.5
+
+print(var_texto)
+
+var_texto=3.5
+
+print(var_texto)
