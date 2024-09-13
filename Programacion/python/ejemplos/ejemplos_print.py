3007ef87d5a8a8ef9cfe49ea98ef674ab3ea03ea
Nilsimon <nil.simon@campus.xaloc.org>
2024-09-13 12:30:30 +0200

    Add files via upload

---
+++ Programacion/python/ejemplos/ejemplos_print.py
@@ -0,0 +1,8 @@
+#2 maneras de trabajar con print
+
+var_texto="hola"
+var_numero=3
+var_num=3.5
+
+#formato 1 de print: poner textos entre comillas separados con comas
+print("el total es:", var_numero, "euros, y el cambio es", var_num)
