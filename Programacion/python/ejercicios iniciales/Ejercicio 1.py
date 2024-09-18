07477b3fdceb007967d837d4f854f129a3ba31e6
Nilsimon <nil.simon@campus.xaloc.org>
2024-09-18 13:24:18 +0200

    Add files via upload

---
+++ Programacion/python/ejercicios iniciales/Ejercicio 1.py
@@ -0,0 +1,6 @@
+#Ejercicio 1:Programa que muestre por pantalla la frase “hello world”
+
+texto1="Hello world"
+
+print(texto1)
+
---
+++ Programacion/python/ejercicios iniciales/Ejercicio 2.py
@@ -0,0 +1,14 @@
+#Ejercicio 2:  Programa que introduzca por teclado tres tipos de variables y se muestren por pantalla
+#en el siguiente orden: número entero, texto y número decimal.
+
+var_numero= 1
+
+var_letra= "a"
+
+var_decimal= 6.8
+
+print("el valor introducido es un:", var_numero)
+
+print(f"el valor introducido es la letra {var_letra}" )
+
+print("el valor introducido es el número decimal:", var_decimal)
