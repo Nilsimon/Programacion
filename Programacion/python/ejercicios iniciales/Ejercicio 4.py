fd693f3a6dddf571ce85150a908e4d88f342f8b4
Nilsimon <nil.simon@campus.xaloc.org>
2024-09-18 13:39:14 +0200

    Add files via upload

---
+++ Programacion/python/ejercicios iniciales/Ejercicio 4.py
@@ -0,0 +1,10 @@
+#Ejercicio 4: A partir del código anterior, realiza una versión para números con decimales
+
+
+numero_1=float(input("Dame un numero decimal:"))
+
+numero_2=float(input("Dame otro numero decimal:"))
+
+total=numero_1+numero_2
+
+print("La suma de los dos valores da como resultado:", total)
