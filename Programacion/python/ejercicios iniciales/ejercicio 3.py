09eefb55246e9fe9f725007613060baed30de8c7
Nilsimon <nil.simon@campus.xaloc.org>
2024-09-18 13:34:51 +0200

    Add files via upload

---
+++ Programacion/python/ejercicios iniciales/ejercicio 3.py
@@ -0,0 +1,11 @@
+#Ejercicio 2: Programa que pida dos números enteros y realice la suma correspondiente
+
+numero_1=int(input("Dame un numero entero:"))
+
+numero_2=int(input("Dame otro numero entero:"))
+
+total=numero_1+numero_2
+
+print("La suma de los dos valores da como resultado:", total)
+
+
