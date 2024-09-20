c6d665f2520418d0468237c248ce6c2ebdc9ec1c
Nilsimon <nil.simon@campus.xaloc.org>
2024-09-20 12:13:51 +0200

    Add files via upload

---
+++ Programacion/python/ejercicios iniciales/ejercicio5.py
@@ -0,0 +1,14 @@
+##Programa que pida cinco palabras y muestre una frase con las cinco. Modifica el código para que entre palabra y palabra haya una coma
+
+palabra1=input("Dame una palabra:")
+
+palabra2=input("Dame otra palabra:")
+
+palabra3=input("Dame otra palabra:")
+
+palabra4=input("Dame otra palabra:")
+
+palabra5=input("Dame otra palabra:")
+
+print(palabra1,",",palabra2,",",palabra3,",",palabra4,",",palabra5)
+
