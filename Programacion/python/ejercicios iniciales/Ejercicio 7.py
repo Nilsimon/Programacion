5798793250e9c0f26231e1415cde7164cc2a6efe
Nilsimon <nil.simon@campus.xaloc.org>
2024-09-20 12:35:44 +0200

    Add files via upload

---
+++ Programacion/python/ejercicios iniciales/Ejercicio 7.py
@@ -0,0 +1,13 @@
+##programa que calcule dos operandos con los 7 operadores vistos en clase. ¿Cómo puedesforzar que el resultado de la división tenga 2 decimales?
+
+var1=float(input("Dame un numero:"))
+
+var2=float(input("Dame otro numero:"))
+
+print("La suma es:", var1+var2)
+
+print("La resta es:", var1-var2)
+
+print("El producto es:", var1*var2)
+
+print("La division es:", round(var1/var2,2))
---
+++ Programacion/python/ejercicios iniciales/ejercicio6.py
@@ -0,0 +1,13 @@
+##A partir del programa 5. Haz que se muestre por pantalla también la frase en el orden inverso en que se han introducido las palabras.
+
+palabra1=input("Dame una palabra:")
+
+palabra2=input("Dame otra palabra:")
+
+palabra3=input("Dame otra palabra:")
+
+palabra4=input("Dame otra palabra:")
+
+palabra5=input("Dame otra palabra:")
+
+print(palabra5,",",palabra4,",",palabra3,",",palabra2,",",palabra1)
