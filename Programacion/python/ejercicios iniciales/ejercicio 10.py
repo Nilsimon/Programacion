d21f26c3a3f22334ae51bdf1cb278bef5b9a2551
Nilsimon <nil.simon@campus.xaloc.org>
2024-09-25 13:38:37 +0200

    Add files via upload

---
+++ Programacion/python/ejercicios iniciales/ejercicio 10.py
@@ -0,0 +1,9 @@
+#Introduce por teclado dos números y muestre por pantalla la siguiente información:
+#cociente, resto y si el dividendo es par o impar
+
+var1=float(input("Introduce un valor:"))
+
+var2=float(input("introduce otro valor"))
+
+print("El cociente es:", var1/var2, "El resto es:", var1-var2)
+
