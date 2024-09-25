a6c255f96e3af25d4393ac711429320fec1ae935
Nilsimon <nil.simon@campus.xaloc.org>
2024-09-25 13:13:24 +0200

    Add files via upload

---
+++ Programacion/python/ejercicios iniciales/ejercico 8.py
@@ -0,0 +1,11 @@
+#programa que pida un número de horas y muestre por pantalla los minutos y segundos
+
+var1= float(input("Dame un número de horas:"))
+
+var2=60
+
+exponente=var2**2
+
+print("El número de horas en minutos es:", var1*var2)
+
+print("El numero de horas en segundos es:", var1*exponente)
