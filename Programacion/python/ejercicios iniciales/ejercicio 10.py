604a6ba680c244a4e2d4aca1d4b5c2b00e93e991
Nilsimon <nil.simon@campus.xaloc.org>
2024-09-27 12:06:18 +0200

    Add files via upload

--- Programacion/python/ejercicios iniciales/ejercicio 10.py
+++ Programacion/python/ejercicios iniciales/ejercicio 10.py
@@ -5,5 +5,14 @@ var1=float(input("Introduce un valor:"))
 
 var2=float(input("introduce otro valor"))
 
-print("El cociente es:", var1/var2, "El resto es:", var1-var2)
+var_coc=var1/var2
+var_rest=var1%var2
+
+print("El cociente es:", var_coc, "El resto es:", var_rest)
+
+if var_rest==0:
+    print("El numero es par")
+
+else:
+    print("El numero es impar")
 
