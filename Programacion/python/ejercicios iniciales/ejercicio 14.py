5000da1d44a1588a4c9d31da685646fa8132440f
Nilsimon <nil.simon@campus.xaloc.org>
2024-09-28 11:05:28 +0200

    Add files via upload

---
+++ Programacion/python/ejercicios iniciales/ejercicio 14.py
@@ -0,0 +1,11 @@
+import math
+var1=float(input("Dime un valor:"))
+
+var_radio=var1/2
+
+var_perim=var1*math.pi
+
+var_area=var_radio**2*math.pi
+
+print("el area es:", f"{var_area:.1f}" ,"y el perimetro:" ,f"{var_perim:.1f}")
+
