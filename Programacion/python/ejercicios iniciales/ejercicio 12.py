efa98c168aa653ec35baa3172bba08e63a1a8a29
Nilsimon <nil.simon@campus.xaloc.org>
2024-09-28 10:17:42 +0200

    Add files via upload

---
+++ Programacion/python/ejercicios iniciales/ejercicio 12.py
@@ -0,0 +1,14 @@
+var_lado=float(input("Dame un valor:"))
+
+var_basemen=float(input("Dame otro valor:"))
+
+var_basemay=float(input("Dame otro valor:"))
+
+var_altura=float(input("Dame otro valor:"))
+
+area= (var_basemen+var_basemay)*var_altura/2
+
+perimetro=var_lado*2+var_basemen+var_basemay
+
+print("El perimetro es:", perimetro, "y el area es:",area)
+
