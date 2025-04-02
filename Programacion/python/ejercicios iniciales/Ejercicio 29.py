7fd8df425fccf8697aa1f7e08e7ce5415cfd6328
Nilsimon <nil.simon@campus.xaloc.org>
2025-04-02 13:12:47 +0200

    BORR

--- Programacion/python/ejercicios iniciales/Ejercicio 29.py
+++
@@ -1,7 +0,0 @@
-#29. Busca Internet qué función permite obtener la longitud de un String, realiza un programa 
-#que al introducir una frase devuelva la longitud
-
-var_frase= input("Introduce una frase:")
-
-print("longitud es", len(var_frase) )
-
