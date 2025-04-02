c7ec60c5de676e308fe5d41b012a08a859283d97
Nilsimon <nil.simon@campus.xaloc.org>
2025-04-02 13:06:41 +0200

    BORRADO

--- Programacion/python/Bucles While/Sin título14.py
+++
@@ -1,9 +0,0 @@
-#57. Realiza un programa que permita adivinar un número comprendido entre 1 y 5. El programa debe controlar si el usuario introduce un número no comprendido entre 1 y 5
-import random
-var1=0
-numeroaleatorio= int(random.randint(1,5))
-while var1!=numeroaleatorio:
-    var1= int(input("Introduce un numero:"))
-    if var1!=numeroaleatorio:
-        print("Incorrecto")
-print("Numero correcto")
